4944820604089f525d65cbb048361d9bb20b1a67
BanKiHyeon <BanKiHyeon@gmail.com>
2024-05-09 22:48:24 +0900

    add script

--- package/python_parameters/setup.py
+++ package/python_parameters/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'minimal_param_node = python_parameters.python_parameters_node:main'
         ],
     },
 )
